0a58cb033c5200a19be530eb1135a69965016cf1
Duzo <james.hall01@outlook.com>
2023-11-16 01:04:35 +0000

    discord theme changing

--- scripts/themes.js
+++ scripts/themes.js
@@ -3,6 +3,9 @@ html.dataset.theme = `theme-light`;
 
 function switchTheme(theme) {
   html.dataset.theme = `theme-${theme}`;
+
+  let discord = document.getElementById("discord-widget");
+  discord.src = "https://discord.com/widget?id=859856751070937098&theme=" + theme;
 }
 function toggleTheme() {
     if (html.dataset.theme == "theme-light") {
